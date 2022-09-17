e97206e396032adfbcfcd588e54d98ef5df7525f
timDeHof <this.is.tim.dehof@gmail.com>
2022-09-17 18:44:17 -0400

    created a shopping cart icon

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,14 @@
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+
+import "./cart-icon.styles.scss";
+
+const CartIcon = () => {
+  return (
+    <div className="cartIcon-container">
+      <ShoppingIcon className="shopping-icon" />
+      <span className="itemCount">0</span>
+    </div>
+  );
+};
+
+export default CartIcon;

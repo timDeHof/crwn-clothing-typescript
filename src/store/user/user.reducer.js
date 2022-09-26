9040d1bba433580140a73773dc206d3376bddcb5
timDeHof <this.is.tim.dehof@gmail.com>
2022-09-25 21:01:04 -0400

    finished migigrating everything to redux and sagas

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -10,14 +10,16 @@ export const userReducer = (state = INITIAL_STATE, action) => {
 
   switch (type) {
     case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
-      return {
-        ...state,
-        currentUser: payload,
-      };
+      return { ...state, currentUser: payload };
+
+    case USER_ACTION_TYPES.SIGN_UP_FAILED:
     case USER_ACTION_TYPES.SIGN_IN_FAILED:
+    case USER_ACTION_TYPES.SIGN_OUT_FAILED:
+      return { ...state, error: payload };
+    case USER_ACTION_TYPES.SIGN_OUT_SUCCESS:
       return {
         ...state,
-        error: payload,
+        currentUser: null,
       };
     default:
       return state;

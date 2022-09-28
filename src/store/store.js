5e9f416adeaa68444f85b362eaae95de16dcf09d
timDeHof <this.is.tim.dehof@gmail.com>
2022-09-27 20:14:10 -0400

    fixed composedEnhancer

--- src/store/store.js
+++ src/store/store.js
@@ -30,9 +30,10 @@ const middleWares = [
 ].filter(Boolean);
 
 const composedEnhancer =
-  process.env.NODE_ENV !== "production" &&
-  window &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
 

6096f888d189f4c7bc3afdabc87fb3b38db5a64d
timDeHof <this.is.tim.dehof@gmail.com>
2022-11-30 22:15:01 -0500

    made app to be PWA

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { Elements } from "@stripe/react-stripe-js";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
+import { PersistGate } from "redux-persist/integration/react"
+import { Elements } from "@stripe/react-stripe-js"
 
-import App from "./App";
+import App from "./App"
 
-import { store, persistor } from "./store/store";
-import { stripePromise } from "./utils/stripe/stripe.utils";
+import { store, persistor } from "./store/store"
+import { stripePromise } from "./utils/stripe/stripe.utils"
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
 
-import "./index.scss";
+import "./index.scss"
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -25,4 +26,6 @@ root.render(
       </PersistGate>
     </Provider>
   </React.StrictMode>
-);
+)
+
+serviceWorkerRegistration.register()
